e7a2b1cf0c195902c31148b041e078637ff7cfa1
stephentcosta <31608698+stephentcosta@users.noreply.github.com>
2017-10-01 22:28:43 -0400

    Plotted from CSV

--- PS02/index.js
+++ PS02/index.js
@@ -2,17 +2,30 @@ var svg = d3.select('svg');
 
 //Create SVG element
 
-svg.append("rect")
+/*svg.append("rect")
     .attr("width", "100%")
     .attr("height", "100%")
-    .attr("fill", "#000");
+    .attr("fill", "#63b6ff");*/
 
-d3.csv("./data.csv", function (myArrayOfObjects){
-        myArrayOfObjects.forEach(function (d){
-          .append("circle")
-          .attr("cx" "," d.x)
-          .attr("cy" "," d.y)
-          .attr("r" "," d.r)
-          .attr("fill" "," d.fill)
-        });
-      });
+//var dataset = d3.csv("./data.csv", function(data) {
+  //  console.log(data);
+
+d3.csv("./data.csv", function (error, circleData) {
+  circleData.forEach(function (d) {
+    d['cx'] = +d['x'];
+    d['cy'] = +d['y'];
+    d['r'] = +d['r'];
+    //d['fill'] = +d['fill'];
+    fill : d.fill;
+  });
+console.log(circleData);
+
+svg.selectAll("circles")
+.data(circleData)
+.enter()
+.append("circle")
+.attr("cx", function (d) { return d.x; })
+.attr("cy", function (d) { return d.y; })
+.attr("r", function (d) { return d.r; })
+.attr("fill", function (d) { return d.fill; })
+});
