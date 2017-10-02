e4ea2a517fe8a61e6d15558178b6969cb7513df2
stephentcosta <31608698+stephentcosta@users.noreply.github.com>
2017-10-01 22:43:57 -0400

    Printed Data to Console

--- PS03/index.js
+++ PS03/index.js
@@ -1,6 +1,10 @@
 var svg = d3.select('svg');
 
-/* Your code goes here */
+//Create SVG element
 
+//var dataset = d3.csv("./data.csv", function(data) {
+  //  console.log(data);
 
-
+d3.csv("bpda.csv", function(data) {
+    console.log(data);
+});
